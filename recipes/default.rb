6fb69c3f36196619361ae75f7bd15c3c54e8b444
jcarapet <jeffcarapetyan@gmail.com>
2014-03-11 10:45:29 -0500

    working installation of home path and unzipping of files. testing on chef-server

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,17 +7,17 @@
 # All rights reserved - Do Not Redistribute
 #
 
-
 # this will create the directory for the machine.
 directory "usr/local/camel" do 
-	mode "0755"
+	mode "0777"
 end
 
 # then it will have to unzip it because RPM's don't exist yet and I haven't figured out ark yet.
 execute "unzip_file" do 
-	cwd "/usr/local/camel" 
-	command "tar -xzvf apache-ant-1.9.3-bin.tar.gz -C /usr/local/camel" 
+	cwd "/vagrant/binaries" 
+	command "tar -xzvf apache-camel-2.12.3.tar.gz -C /usr/local/camel" 
 	action :run
 end
 
 # then we will have to point to the include_recipe path for the machine.
+include_recipe "df_camel::set_camel_home"
